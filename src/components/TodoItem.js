6e87a794c79866f449c2dcb4a0a4d1d9f010a56d
Roman Meredith <romanmeredith@gmail.com>
2019-10-23 01:59:53 -0600

    strike-through

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,16 +2,21 @@ import React from 'react'
 
 
 function TodoItem(props){
+    const pStyle={
+        fontStyle: "italic",
+        color: "grey",
+        textDecoration: 'line-through'
+    }
     
     return(
         
     <div className="todo-item">
         <input 
-        type="checkbox" 
-        checked = {props.item.completed} 
-        onChange={()=>props.handleChange(props.item.id)}
+            type="checkbox" 
+            checked = {props.item.completed} 
+            onChange={()=>props.handleChange(props.item.id)}
         />
-        <p>{props.item.placeholder}</p>
+        <p style={props.item.completed? pStyle : {}}>{props.item.placeholder}</p>
     </div>)
 }
 export default TodoItem
